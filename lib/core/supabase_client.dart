bf749e5e5bba0dc689e96f5c36e22ea84ebc22a5
Sufyan Khan <sufyankhan2004@gmail.com>
2025-12-16 21:50:36 +0500

    Updated project with new features and fixes

--- lib/core/supabase_client.dart
+++ lib/core/supabase_client.dart
@@ -1,31 +1,22 @@
-import 'package:flutter/foundation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AppSupabase {
   static final SupabaseClient client = Supabase.instance.client;
 
+  
   static Future<void> init() async {
-    const supabaseUrl = String.fromEnvironment('https://wftwgjueaummssyqkcmc.supabase.co');
-    const supabaseAnonKey = String.fromEnvironment('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmdHdnanVlYXVtbXNzeXFrY21jIiwicm9sZSI6ImFub');
+  const supabaseUrl = 'https://wftwgjueaummssyqkcmc.supabase.co';
+  const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmdHdnanVlYXVtbXNzeXFrY21jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4MDkyMTIsImV4cCI6MjA4MTM4NTIxMn0.AQ5x3O9sljTDEDZd9dgQEC1ATkhaWREBBDCg2XHxDnE';
 
-    if (supabaseUrl.isEmpty || supabaseAnonKey.isEmpty) {
-      throw StateError(
-        'Missing Supabase credentials. Provide them with --dart-define=SUPABASE_URL=... '
-        'and --dart-define=SUPABASE_ANON_KEY=... when starting the app.',
-      );
-    }
-
-    if (kDebugMode) {
-      debugPrint('Connecting to Supabase project: $supabaseUrl');
-    }
-
-    await Supabase.initialize(
-      url: supabaseUrl,
-      anonKey: supabaseAnonKey,
-    );
-  }
+  await Supabase. initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+  );
+}
+   
 }
 /*flutter run -d chrome `
 flutter run -d chrome `
   --dart-define=SUPABASE_URL=https://wftwgjueaummssyqkcmc.supabase.co `
   --dart-define=SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmdHdnanVlYXVtbXNzeXFrY21jIiwicm9sZSI6ImFub" */
+
